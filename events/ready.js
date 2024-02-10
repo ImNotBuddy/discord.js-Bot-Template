7a8b9184f25388236c53d7a542b7df31fde83e83
Buddy <122407012+ImNotBuddy@users.noreply.github.com>
2024-02-10 22:47:33 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require("discord.js");
+const { ActivityType, Events } = require("discord.js");
 
 module.exports = {
     event: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setActivity({ name: "any command(s)", type: ActivityType.Listening });
         console.log(`${client.user.tag} has successfully logged in.`);
     },
 };
